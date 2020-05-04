a8707026bdb2da94c27f382f00f67a9420170c00
bruno takashi <bt.tengan@gmail.com>
2020-05-04 20:29:01 -0300

    Movimentação jogador e Gerador meteoros

---
+++ Dinomite/Assets/Scripts/Meteor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour {
+	[SerializeField] [Min(0)] float _speed = 0;
+	[SerializeField] [Min(0)] int _damage = 0;
+	Rigidbody2D _rb;
+
+	void Start() {
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		Vector2 target = (Vector2)transform.position + Vector2.down * (_speed * Time.fixedDeltaTime);
+		_rb.MovePosition(target);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			other.GetComponent<PlayerLife>().DealDamage(_damage);
+			Explode();
+		}
+		if (other.gameObject.tag == "Ground") {
+			Explode();
+		}
+	}
+
+	void Explode() {
+		Destroy(gameObject);
+	}
+}
---
+++ Dinomite/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] [Min(0)] float _speed = 0;
+	Rigidbody2D _rb;
+	Controls _controls;
+	Vector2 _input;
+
+	void Start() {
+		_rb = GetComponent<Rigidbody2D>();
+		_controls = new Controls();
+		_controls.Player.Enable();
+	}
+
+	void Update() {
+		_input = _controls.Player.Move.ReadValue<Vector2>();
+	}
+
+	void FixedUpdate() {
+		if (_input.x != 0) {
+			Vector2 target = (Vector2)transform.position + Vector2.right * (_input.x * _speed * Time.fixedDeltaTime);
+			_rb.MovePosition(target);
+		}
+	}
+
+	void OnDisable() {
+		_controls.Player.Disable();
+	}
+}
