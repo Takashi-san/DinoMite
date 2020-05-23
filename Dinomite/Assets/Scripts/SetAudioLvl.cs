12821643ee3fd820ca4f8cb41c63b4169f310d1e
bruno takashi <bt.tengan@gmail.com>
2020-05-22 21:16:49 -0300

    Pause e controle de volume básico

---
+++ Dinomite/Assets/Scripts/SetAudioLvl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetAudioLvl : MonoBehaviour {
+	[SerializeField] AudioMixer _mixer = null;
+	[SerializeField] string _variable = null;
+
+	public void SetLvl(float level) {
+		_mixer.SetFloat(_variable, level);
+	}
+}
